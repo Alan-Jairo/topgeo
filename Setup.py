7c1236f1c389a452acd42e92cd6b07310583e927
Alan-Jairo <alanotero96@hotmail.com>
2017-01-10 22:48:52 -0600

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-from  setuptools import setup
+from  distutils.core import setup
 
-setup(	name="topgeo",
-		version="0.1",
-		description="Herramienta para realizar calculos topograficos",
-		url="http://Github.com/Alan-Jairo",
-		author="Alan Cruz - Jairo Avalos",
-		author_email="alanotero96@hotmail.com,javalos@ucol.mx",
-		license="MIT",
-		packages=["coordenada","distancia"])
+setup(	
+	name="topgeo",
+	version="0.1",
+	long_description="Herramienta para realizar calculos topograficos",
+	license="MIT",
+	packages=["coordenada","distancia"])
