a529c3a519790d9dad0f146fd7487a65806f5334
Alan-Jairo <alanotero96@hotmail.com>
2017-01-12 11:34:52 -0600

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from  distutils.core import setup
-      """
-      Esta libreria funciona para realizar calculos topograficos.
-      """
+      
 
 setup(	
 	name="topgeo",
