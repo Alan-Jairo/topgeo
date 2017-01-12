862668cba34a17ec367e8bbce5c981c6616b30d6
Alan-Jairo <alanotero96@hotmail.com>
2017-01-12 15:38:19 -0600

    Update __init__.py

--- topgeo/__init__.py
+++ topgeo/__init__.py
@@ -1,7 +1,7 @@
 """ Esta libreria funciona para realizar calculos topograficos
 
-<topgeo.calcoor("csv")> #Calcula las coordenadas (x,y,z)
-<topgeo.caldist("csv")> """
+<topgeo.calcoor("csv")> #Calcula las coordenadas (x,y,z) de un 'csv'.
+<topgeo.caldist("csv")> #Calcula las distancias horizontales e inclinadas."""
 
 
 from coordenada import calcoor
