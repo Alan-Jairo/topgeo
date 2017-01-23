fd5f864e794246637d4c45bc1bce26d443f56722
Alan-Jairo <alanotero96@hotmail.com>
2017-01-23 01:19:25 -0600

    Update topgeo.py

--- topgeo/topgeo.py
+++ topgeo/topgeo.py
@@ -14,38 +14,13 @@ def calcoor(csv):
     """
     
     
-    # Agregar los modulos numpy, Tkinter y pandas 
+    # Agregar los modulos numpy, easygui y pandas 
     import numpy as np
     import pandas as pd
-    from Tkinter import *
-    from tkinter.filedialog import askopenfilename
-    from tkinter.messagebox import showerror
+    import easygui
     
-    class MyFrame(Frame):
-    def __init__(self):
-        Frame.__init__(self)
-        self.master.title("Example")
-        self.master.rowconfigure(5, weight=1)
-        self.master.columnconfigure(5, weight=1)
-        self.grid(sticky=W+E+N+S)
-
-        self.button = Button(self, text="Browse", command=self.load_file, width=10)
-        self.button.grid(row=1, column=0, sticky=W)
-
-    def load_file(self):
-        fname = askopenfilename(filetypes=(("Template files", "*.tplate"),
-                                           ("HTML files", "*.html;*.htm"),
-                                           ("All files", "*.*") ))
-        if fname:
-            try:
-                print("""here it comes: self.settings["template"].set(fname)""")
-            except:                     # <- naked except is a bad idea
-                showerror("Open Source File", "Failed to read file\n'%s'" % fname)
-            return
-
-
-         if __name__ == "__main__":
-    MyFrame().mainloop()
+    csv = easygui.fileopenbox()
+    
     
 
     # Almacenamos en la variable pts el archivo csv
