e80f2bf0351cbdaecb92e072f4d7e72787c0bad7
Alan-Jairo <alanotero96@hotmail.com>
2017-01-23 13:30:53 -0600

    Update __init__.py

--- topgeo/__init__.py
+++ topgeo/__init__.py
@@ -1,8 +1,10 @@
 """ Esta libreria funciona para realizar calculos topograficos
 
 <topgeo.calcoor("argumento")> #Calcula las coordenadas (x,y,z) de un.
-<topgeo.caldist("argumento")> #Calcula las distancia horizontal entre dos puntos."""
+<topgeo.caldist("argumento")> #Calcula las distancia horizontal entre dos puntos.
+<topgeo.caldist3d("argumento")> #Calculo de distancias entre dos puntos arbitrarios en x,y,z."""
 
 
 from coordenada import calcoor
 from distancia import caldist
+
