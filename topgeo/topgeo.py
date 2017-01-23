b8c9b039661d2f45b3f7e394c6a2e5bd67e2d07d
Alan-Jairo <alanotero96@hotmail.com>
2017-01-22 22:55:06 -0600

    Update topgeo.py

--- topgeo/topgeo.py
+++ topgeo/topgeo.py
@@ -29,7 +29,7 @@ def calcoor(csv):
     Button(text='Abrir Archivo', command=llamada).pack(fill=mainloop()
     
     #Almacenamos en la variable pts el archivo csv
-    pts = pd.read_csv(llamada)
+    pts = pd.read_csv(csv)
     
     #Se almacenan las variables en las columnas que queremos del csv
     DH = pts['Dist_H']
