2d0285447c2474b7d3b1c60cfdcce51e42306315
Alan-Jairo <alanotero96@hotmail.com>
2017-01-23 13:42:57 -0600

    Update distancia3d.py

--- topgeo/distancia3d.py
+++ topgeo/distancia3d.py
@@ -2,7 +2,7 @@ def caldist3d(X1, Y1, Z1, X2, Y2, Z2):
     """
     Esta funcion sirve para realizar el calculo de distancias entre dos puntos arbitrarios.
     """
-    # Importamos los modulos numpy y pandas
+    # Importamos el modulo numpy
     import numpy as np
 
     n = (((X1-X2)**2)+((Y1-Y2)**2)+(Z1-Z2))
